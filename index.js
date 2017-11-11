cd280abce7d9c10a7b47105318a59c5607725a52
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-11-11 19:05:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,15 +13,4 @@ function ternaryCheckCity(city){
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(tip){
-  switch (tip){
-    case 'generous':
-      return 'Thank you so much.';
-      break;
-    case 'not as generous':
-      return 'Thank you.';
-      break;
-    case default:
-      return 'Bye'
-  }
-}
+
