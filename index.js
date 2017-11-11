0dac18662dd6a07434a37316b62d17365772f725
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-11-11 19:09:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,8 @@ function switchOnCharmFromTip(tip){
     case 'generous':
       return 'Thank you so much.';
       break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
   }
 }
